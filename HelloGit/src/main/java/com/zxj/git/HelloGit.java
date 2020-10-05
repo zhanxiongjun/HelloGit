ae8067065627492a6c7045b5c79f8becd613fb9d
zhanxiongjun <zhanj0123@gmail.com>
2020-10-05 18:25:02 +0800

    创建分支

--- HelloGit/src/main/java/com/zxj/git/HelloGit.java
+++ HelloGit/src/main/java/com/zxj/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git！");
         System.out.println("版本1！");
+        System.out.println("创建分支");
     }
 }
